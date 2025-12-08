2ff4617dc403eeedad4d6063b4afa0b20c8cd6f3
ImMasterSam <sam9630147@gmail.com>
2025-12-08 20:50:35 +0800

    Add arguments parser

--- part2/frozen_lake.py
+++ part2/frozen_lake.py
@@ -3,6 +3,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pickle
 import sys
+import argparse
+from tqdm import tqdm
+
+TRAIN_EPISODES = 15000
+RUN_EPISODES = 1000
+
+parser = argparse.ArgumentParser()
+group = parser.add_mutually_exclusive_group()
+group.add_argument('-r',
+                   '--run',
+                   action="store_true",
+                   help="執行已訓練好的模型")
+group.add_argument('-t',
+                   '--train',
+                   action="store_true",
+                   help="利用超參數訓練模型")
+group.add_argument('-tu',
+                   '--tune',
+                   action="store_true",
+                   help="自動尋找最佳參數組合")
 
 def print_success_rate(rewards_per_episode):
     """Calculate and print the success rate of the agent."""
@@ -15,26 +35,28 @@ def print_success_rate(rewards_per_episode):
 def find_best_combination():
     "自動的尋找最佳參數組合"
     best_late_success_rate = 0
-    best_combination = (0,0)
+    best_combination = (0,0,0)
 
     decay_rate = np.arange(0.00008, 0.0004, 0.00001)
     min_epsilon_values = np.arange(0, 0.00025, 0.00001)
+    learning_rates = np.arange(0.5, 0.9, 0.05)
 
     for epsilon_decay_rate in decay_rate:
         for min_epsilon in min_epsilon_values:
-            print(f"Testing combination: epsilon_decay_rate={epsilon_decay_rate}, min_epsilon={min_epsilon}")
-            late_success_rate , q_table = run(15000, is_training=True, render=False, epsilon_decay_rate=epsilon_decay_rate, min_epsilon=min_epsilon)
-            if late_success_rate > best_late_success_rate:
-                best_late_success_rate = late_success_rate
-                best_combination = (epsilon_decay_rate, min_epsilon)
+            for learning_rate in learning_rates:
+                print(f"Testing combination: epsilon_decay_rate={epsilon_decay_rate}, min_epsilon={min_epsilon}, learning_rate={learning_rate}")
+                late_success_rate , q_table = run(15000, is_training=True, render=False, epsilon_decay_rate=epsilon_decay_rate, min_epsilon=min_epsilon, learning_rate_a=learning_rate)
+                if late_success_rate > best_late_success_rate:
+                    best_late_success_rate = late_success_rate
+                    best_combination = (epsilon_decay_rate, min_epsilon, learning_rate)
 
-                print(f"New best combination found: epsilon_decay_rate={epsilon_decay_rate}, min_epsilon={min_epsilon} with Success Rate: {best_late_success_rate:.2f}%")
-                f = open("frozen_lake8x8.pkl","wb")
-                pickle.dump(q_table, f)
-                f.close()
-    print(f"Best combination: epsilon_decay_rate={best_combination[0]:.5f}, min_epsilon={best_combination[1]:.5f} with Success Rate: {best_late_success_rate:.2f}%")
+                    print(f"New best combination found: epsilon_decay_rate={epsilon_decay_rate}, min_epsilon={min_epsilon}, learning_rate={learning_rate} with Success Rate: {best_late_success_rate:.2f}%")
+                    f = open("frozen_lake8x8.pkl","wb")
+                    pickle.dump(q_table, f)
+                    f.close()
+    print(f"Best combination: epsilon_decay_rate={best_combination[0]:.5f}, min_epsilon={best_combination[1]:.5f}, learning_rate={best_combination[2]:.2f}  with Success Rate: {best_late_success_rate:.2f}%")
 
-def run(episodes, is_training=True, render=False , epsilon_decay_rate=0.0001 , min_epsilon=0.0001):
+def run(episodes, is_training=True, render=False , epsilon_decay_rate=0.0001 , min_epsilon=0.0001, learning_rate_a=0.55):
 
     env = gym.make('FrozenLake-v1', map_name="8x8", is_slippery=True, render_mode='ansi' if render else None)
 
@@ -45,14 +67,14 @@ def run(episodes, is_training=True, render=False , epsilon_decay_rate=0.0001 , m
         q = pickle.load(f)
         f.close()
 
-    learning_rate_a = 0.9 # alpha or learning rate
+    # learning_rate_a = 0.9 # alpha or learning rate
     discount_factor_g = 0.9 # gamma or discount rate. Near 0: more weight/reward placed on immediate state. Near 1: more on future state.
     epsilon = 1         # 1 = 100% random actions
     rng = np.random.default_rng()   # random number generator
 
     rewards_per_episode = np.zeros(episodes)
 
-    for i in range(episodes):
+    for i in tqdm(range(episodes), desc="Episodes", leave=False):
         state = env.reset()[0]  # states: 0 to 63, 0=top left corner,63=bottom right corner
         
         terminated = False      # True when fall in hole or reached goal
@@ -102,8 +124,13 @@ def run(episodes, is_training=True, render=False , epsilon_decay_rate=0.0001 , m
         return late_success_rate , q
 
 if __name__ == '__main__':
-    #0 train 1 test
-    if len(sys.argv) > 1 and sys.argv[1] == '0':
+    
+    # Get and parse the arguments
+    args = parser.parse_args()
+
+    if args.run:
+        run(RUN_EPISODES, is_training=False, render=False)
+    elif args.train:
+        run(TRAIN_EPISODES, is_training=True, render=False)
+    elif args.tune:
         find_best_combination()
-    else:
-        run(500, is_training=False, render=True)
